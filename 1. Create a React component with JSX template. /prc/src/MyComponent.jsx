35a6c5d83b01c21c16505a21629da06b061870d5
Atharv Naik <114814921+atharvnaik07@users.noreply.github.com>
2025-01-26 15:32:38 +0530

    done

---
+++ 1. Create a React component with JSX template. /prc/src/MyComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+ function MyComponent() {
+return (
+<div>
+<h1>Welcome to My Component!</h1>
+<p>This is a simple React component.</p>
+</div>
+);
+}
+ export default MyComponent;
